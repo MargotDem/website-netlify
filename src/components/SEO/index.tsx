73c890918d8f885302b325912b7aace6dc637f77
margot <margot@ovrsea.com>
2019-11-30 18:52:38 +0100

    load images in js

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,87 +1,108 @@
-/* eslint-disable prettier/prettier */
-import React from "react";
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+import React, { Component } from "react";
 import Helmet from "react-helmet";
 import config from "../../../data/SiteConfig";
 
-const Index = ({ data: {
-  title,
-  path,
-  description,
-  image,
-  openGraphType,
-  twitterCard,
-} }) => {
-  const pageTitle = title ? title : config.siteTitle
-  const pageDescription = description ? description : config.siteDescription
-  const pageUrl = path ? config.siteUrl + path : config.siteUrl
-  const pageImagePath = image ? image.imagePath : config.siteDefaultImage
-
-  const schemaOrgJSONLD = {
-    "@context": "http://schema.org",
-    "@type": "WebSite",
-    "url": pageUrl,
-    "name": pageTitle,
-    "publisher": {
-      "@type": "Organization",
-      "name": config.siteTitle,
-      "logo": config.siteUrl + config.siteLogo
-    },
-    "mainEntityOfPage": {
-      "@type": "WebPage",
-      "@id": pageUrl
-    },
-    "description": pageDescription
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageImagePath: null
+    };
   }
 
-  return (
-    <Helmet>
-      {/* general */}
+  componentDidMount() {
+    let {
+      data: { image }
+    } = this.props;
+    const pageImagePath = image ? image.imagePath : config.siteDefaultImage;
+    import(`../../../static${pageImagePath}`).then(image => {
+      this.setState({ pageImagePath: image.default });
+    });
+  }
 
-      <title>{title ? title + " - " + config.siteTitle : config.siteTitle}</title>
-      <meta name="description" content={pageDescription} />
-      <link rel="canonical" href={pageUrl} />
+  render() {
+    let {
+      data: { title, path, description, image, openGraphType, twitterCard }
+    } = this.props;
+    let { pageImagePath } = this.state;
 
-      {/* Schema.org */}
-      <script type="application/ld+json">
-        {JSON.stringify(schemaOrgJSONLD)}
-      </script>
-      {/* wip */}
+    const pageTitle = title ? title : config.siteTitle;
+    const pageDescription = description ? description : config.siteDescription;
+    const pageUrl = path ? config.siteUrl + path : config.siteUrl;
 
-      {/* OpenGraph */}
+    const schemaOrgJSONLD = {
+      "@context": "http://schema.org",
+      "@type": "WebSite",
+      url: pageUrl,
+      name: pageTitle,
+      publisher: {
+        "@type": "Organization",
+        name: config.siteTitle,
+        logo: config.siteUrl + config.siteLogo
+      },
+      mainEntityOfPage: {
+        "@type": "WebPage",
+        "@id": pageUrl
+      },
+      description: pageDescription
+    };
 
-      <meta property="og:type" content={openGraphType ? openGraphType : "website"} />
-      {/* website par defaut, sauf si article, news, video.. */}
-      <meta property="og:title" content={pageTitle} />
-      {/* title de la page */}
-      <meta property="og:description" content={pageDescription} />
-      {/* description de la page */}
-      <meta property="og:url" content={pageUrl} />
-      {/* url de la page */}
-      <meta
-        property="og:image"
-        content={pageImagePath}
-      />
-      <meta
-        property="og:image:alt"
-        content={image ? image.imageAlt : config.siteDefaultImageAlt}
-      />
-      {/* pour la home une image custom (rectangle rouge 1200 X 630, pour les
-        autres une image spécifique pour chaque contenu */}
+    return (
+      <Helmet>
+        {/* general */}
 
-      {/* Twitter Card */}
+        <title>
+          {title ? title + " - " + config.siteTitle : config.siteTitle}
+        </title>
+        <meta name="description" content={pageDescription} />
+        <link rel="canonical" href={pageUrl} />
 
-      <meta name="twitter:card" content={twitterCard ? twitterCard : "summary"} />
-      {/* summary par defaut, summary_large_image pour les autres articles, news.. */}
-      <meta name="twitter:title" content={pageTitle} />
-      {/* title de la page */}
-      <meta name="twitter:description" content={pageDescription} />
-      {/* description de la page */}
-      <meta name="twitter:url" content={pageUrl} />
-      {/* url de la page */}
-      <meta name="twitter:image" content={pageImagePath} />
-      {/* pour la home une image custom, pour les autres une image spécifique pour chaque contenu */}
-    </Helmet>
-  );
-};
+        {/* Schema.org */}
+        <script type="application/ld+json">
+          {JSON.stringify(schemaOrgJSONLD)}
+        </script>
+        {/* wip */}
+
+        {/* OpenGraph */}
+
+        <meta
+          property="og:type"
+          content={openGraphType ? openGraphType : "website"}
+        />
+        {/* website par defaut, sauf si article, news, video.. */}
+        <meta property="og:title" content={pageTitle} />
+        {/* title de la page */}
+        <meta property="og:description" content={pageDescription} />
+        {/* description de la page */}
+        <meta property="og:url" content={pageUrl} />
+        {/* url de la page */}
+        <meta property="og:image" content={pageImagePath} />
+        <meta
+          property="og:image:alt"
+          content={image ? image.imageAlt : config.siteDefaultImageAlt}
+        />
+        {/* pour la home une image custom (rectangle rouge 1200 X 630, pour les
+              autres une image spécifique pour chaque contenu */}
+
+        {/* Twitter Card */}
+
+        <meta
+          name="twitter:card"
+          content={twitterCard ? twitterCard : "summary"}
+        />
+        {/* summary par defaut, summary_large_image pour les autres articles, news.. */}
+        <meta name="twitter:title" content={pageTitle} />
+        {/* title de la page */}
+        <meta name="twitter:description" content={pageDescription} />
+        {/* description de la page */}
+        <meta name="twitter:url" content={pageUrl} />
+        {/* url de la page */}
+        <meta name="twitter:image" content={pageImagePath} />
+        {/* pour la home une image custom, pour les autres une image spécifique pour chaque contenu */}
+      </Helmet>
+    );
+  }
+}
 
 export default Index;
--- src/components/contents/simpleContent.tsx
+++ src/components/contents/simpleContent.tsx
@@ -1,27 +1,54 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+import React, { Component } from "react";
 import unified from "unified";
 import parse from "remark-parse";
 import remark2react from "remark-react";
 
-const SimpleContent = ({ content }) => {
-  return (
-    <div className="container has-text-centered simple-content">
-      <div className="columns">
-        <div className="column is-8 is-offset-2">
-          <h1 className="subtitle">{content.title}</h1>
-          <div className="simple-content-description">
-            {
-              unified()
-                .use(parse)
-                .use(remark2react)
-                .processSync(content.description).contents
-            }
+class SimpleContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { imagePath: null };
+  }
+
+  componentDidMount() {
+    let {
+      content: {
+        image: { imagePath }
+      }
+    } = this.props;
+    import(`../../../static${imagePath}`).then(result => {
+      this.setState({ imagePath: result.default });
+    });
+  }
+
+  render() {
+    let { content } = this.props;
+    let { image: { imageAlt }} = content;
+    let { imagePath } = this.state;
+
+    return (
+      <div className="container has-text-centered simple-content">
+        <div className="columns">
+          <div className="column is-8 is-offset-2">
+            <h1 className="subtitle">{content.title}</h1>
+            <div className="simple-content-description">
+              {
+                unified()
+                  .use(parse)
+                  .use(remark2react)
+                  .processSync(content.description).contents
+              }
+            </div>
+            <img
+              alt={imageAlt}
+              src={imagePath}
+              className="simple-content-image"
+            />
           </div>
-          <img alt="" src={content.image} className="simple-content-image" />
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SimpleContent;
